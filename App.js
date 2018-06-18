b9acfd141546d026d14cf9785555d76eba5e9d4e
Richard Pham <rpham@localhost.localdomain>
2018-06-18 17:02:36 -0600

    Add Top Bar Buttons
    Implementation of high fidelity prototypes of the search and cart buttons.

--- App.js
+++ App.js
@@ -1,24 +1,20 @@
 import React from 'react';
 import {Alert, ActivityIndicator, Button, Linking, StyleSheet, ScrollView, Text, TouchableOpacity, View, Image} from 'react-native';
 import {createStackNavigator} from 'react-navigation';
-import {FontAwesome} from '@expo/vector-icons';
+import { FontAwesome,Ionicons, Icon} from '@expo/vector-icons';
+import HeaderButtons from 'react-navigation-header-buttons';
 
 class HomeScreen extends React.Component {
-  static navigationOptions =({navigation})=> ({
+  static navigationOptions = {
     title: 'Home',
-    headerRight:(
-      <TouchableOpacity
-        style={{
-          marginRight:10
-        }}
-        onPress={() => navigation.navigate('Browse')}>
-        <FontAwesome name='search'
-          size={35}
-          color='#000'
-        />
-      </TouchableOpacity>
-    )
-  });
+    headerRight: (
+      <HeaderButtons IconComponent={Ionicons} OverflowIcon={<Ionicons name="ios-more" size={23} color="blue" />} iconSize={23} color="blue">
+        <HeaderButtons.Item title="add" iconName="ios-cart" onPress={() => console.warn('add')} />
+        <HeaderButtons.Item title="add" iconName="ios-search" onPress={() => console.warn('add')} />
+        <HeaderButtons.Item title="select" onPress={() => console.warn('edit')} />
+      </HeaderButtons>
+    ),
+  };
 
   _onPressButton() {
     Alert.alert('You tapped the button!');
@@ -59,21 +55,21 @@ class HomeScreen extends React.Component {
             <FontAwesome name="twitter-square"
               size={50}
               color='#1DA1F2'
-              onPress={() => Linking.openURL('https://twitter.com/FairyTalesYYC')}
+              onPress={() => {Linking.openURL('https://twitter.com/FairyTalesYYC')}}
             />
             <FontAwesome name="facebook-square"
               size={50}
               color='#3B5998'
-              onPress={() => Linking.openURL('https://www.facebook.com/fairyfilmfestyyc')}
+              onPress={() => {Linking.openURL('https://www.facebook.com/fairyfilmfestyyc')}}
             />
             <FontAwesome name="instagram"
               size={50}
-              onPress={() => Linking.openURL('https://www.instagram.com/fairytalesfilmfest/')}
+              onPress={() => {Linking.openURL('https://www.instagram.com/fairytalesfilmfest/')}}
             />
             <FontAwesome name="vimeo-square"
               size={50}
               color='#1ab7ea'
-              onPress={() => Linking.openURL('https://vimeo.com/fairytalesqueerfilmfest')}
+              onPress={() => {Linking.openURL('https://vimeo.com/fairytalesqueerfilmfest')}}
             />
 
           </View>
@@ -86,6 +82,13 @@ class HomeScreen extends React.Component {
 class AboutScreen extends React.Component {
   static navigationOptions = {
     title: 'About',
+    headerRight: (
+      <HeaderButtons IconComponent={Ionicons} OverflowIcon={<Ionicons name="ios-more" size={23} color="blue" />} iconSize={23} color="blue">
+        <HeaderButtons.Item title="add" iconName="ios-cart" onPress={() => console.warn('add')} />
+        <HeaderButtons.Item title="add" iconName="ios-search" onPress={() => console.warn('add')} />
+        <HeaderButtons.Item title="select" onPress={() => console.warn('edit')} />
+      </HeaderButtons>
+    ),
   };
 
   render() {
@@ -123,7 +126,7 @@ class MovieButton extends React.Component {
 
   render() {
     let pic = {
-      uri:this.props.imguri + this.props.datum.movie.posterthumb
+      uri:'https://s3-us-west-1.amazonaws.com/cqasimagehost' + this.props.datum.movie.posterthumb
     };
     return (
       <View
@@ -176,6 +179,13 @@ class BrowseScreen extends React.Component {
 
   static navigationOptions = {
     title: 'Browse',
+    headerRight: (
+      <HeaderButtons IconComponent={Ionicons} OverflowIcon={<Ionicons name="ios-more" size={23} color="blue" />} iconSize={23} color="blue">
+        <HeaderButtons.Item title="add" iconName="ios-cart" onPress={() => console.warn('add')} />
+        <HeaderButtons.Item title="add" iconName="ios-search" onPress={() => console.warn('add')} />
+        <HeaderButtons.Item title="select" onPress={() => console.warn('edit')} />
+      </HeaderButtons>
+    ),
   };
 
   componentDidMount() {
@@ -201,19 +211,8 @@ class BrowseScreen extends React.Component {
 
   renderMovieButton(datum) {
     return (
-      <TouchableOpacity
-        key={datum.movie.id}
-        onPress={
-          () =>  this.props.navigation.navigate(
-            'Movie',
-            {
-              datum: datum,
-              movietitle: datum.movie.title,
-              imguri: this.state.imguri
-            }
-          )
-        } >
-        <MovieButton datum={datum} imguri={this.state.imguri} />
+      <TouchableOpacity key={datum.movie.id} onPress={() =>  this.props.navigation.navigate('Movie')}>
+        <MovieButton datum={datum}/>
       </TouchableOpacity>
     );
   }
@@ -242,21 +241,16 @@ class BrowseScreen extends React.Component {
     );
   }
 }
-
 class MovieScreen extends React.Component{
-  static navigationOptions = ({ navigation }) => {
-    return {
-      title: navigation.getParam('movietitle', 'Movie')
-    };
+  static navigationOptions = {
+    title: 'MovieNameShouldBeHere',
+    headerRight: (
+      <HeaderButtons IconComponent={Ionicons} OverflowIcon={<Ionicons name="ios-more" size={23} color="blue" />} iconSize={23} color="blue">
+        <HeaderButtons.Item title="add" iconName="ios-cart" onPress={() => console.warn('add')} />
+      </HeaderButtons>
+    ),
   };
-
   render() {
-    const datum = this.props.navigation.getParam('datum');
-    const imguri = this.props.navigation.getParam('imguri');
-    const pic = {
-      uri: imguri + datum.movie.poster
-    };
-
     return (
       <ScrollView>
         <View
@@ -265,7 +259,7 @@ class MovieScreen extends React.Component{
             height: 154,
             padding: 20,
           }}>
-          <Image source={pic} style={{
+          <Image source={require('./backgroundLogo.png')} style={{
             height: '100%',
             width: '30%',
             padding: 20
@@ -276,7 +270,13 @@ class MovieScreen extends React.Component{
             height: '100%',
             width: '70%'
           }}>
-            {datum.movie.plotfull}
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat
+            nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           </Text>
         </View>
         <View style={{
@@ -332,6 +332,18 @@ class MovieScreen extends React.Component{
   }
 }
 
+const RootStack = createStackNavigator(
+  {
+    Home: HomeScreen,
+    About: AboutScreen,
+    Browse: BrowseScreen,
+    Movie: MovieScreen,
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -355,22 +367,9 @@ const styles = StyleSheet.create({
   }
 },);
 
-
-const RootStack = createStackNavigator(
-  {
-    Home: HomeScreen,
-    About: AboutScreen,
-    Browse: BrowseScreen,
-    Movie: MovieScreen,
-  },
-  {
-    initialRouteName: 'Home',
-    // headerMode: 'none'
-  }
-);
-
-export default class App extends React.Component {
+export default createStackNavigator({
   render() {
     return <RootStack />;
-  }
-}
+  }},
+{ headerMode: 'none' }
+);
